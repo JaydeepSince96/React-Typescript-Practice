fa9b699421284c5653c38b6be397a4836ca71ef6
Jaydeep Bhattacharya <vedlogic1@ligtwayfinance.com>
2025-05-09 11:47:28 +0530

    necessary pacakage installed to initiate the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-200 h-full w-full'>
+        <Button title='Button' />
+      </div>
+    </>
+  )
+}
+
+export default App
